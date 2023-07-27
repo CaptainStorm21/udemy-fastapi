ccae40722f84ec2b4ca98db1d5eadce18ff70362
Captain Storm <CaptainStorm21@users.noreply.github.com>
2023-07-26 18:39:32 -0700

    11  query parameters

--- main.py
+++ main.py
@@ -3,14 +3,23 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-# static pathes must be ahead of dynamic path
 @app.get('/user/admin')
 def admin():
     return {'This is admin page'}
 
 
-# dynamic path
+
 @app.get('/user/{username}')
 def profile(username):
     return {f'This is a profile page for {username}'}
+
+# http://127.0.0.1:8000/cities?id=paris
+@app.get('/cities')
+def profile(id:str):
+    return {f'This is a profile page for {id}'}
+
+# http://127.0.0.1:8000/province?province=tuscany&&hours=20
+@app.get('/province')
+def province(province:str, hours:int):
+    return {f'This is a province page for {province} and it takes to get there {hours}hours '}
 

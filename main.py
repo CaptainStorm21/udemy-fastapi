0003b9c60bbc5ac091cc6785e9c7214acd4e7c4c
Captain Storm <CaptainStorm21@users.noreply.github.com>
2023-07-26 17:58:39 -0700

    7-path parameter

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ app = FastAPI()
 def index():
     return 'Hello world!'
 
+@app.get('/property')
+def property():
+    return 'this is a property page'
+
+@app.get('/movies')
+def  movies():
+    return {'movie list': {'movie 1', 'movie2', 'movie3'}}

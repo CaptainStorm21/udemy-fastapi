dd0d9f969ab4e48690819cbeffd3fbb207544245
Captain Storm <CaptainStorm21@users.noreply.github.com>
2023-07-26 18:07:10 -0700

    7 path paramteters

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ def index():
 def property():
     return 'this is a property page'
 
+# id here is a path paramter
+@app.get('/property/{id}')
+def property(id):
+    return {f'this is a property {id}'}
+
+@app.get('/vacation/1')
+def property():
+    return 'this is a vacation page for user1'
+
+
 @app.get('/movies')
 def  movies():
     return {'movie list': {'movie 1', 'movie2', 'movie3'}}
